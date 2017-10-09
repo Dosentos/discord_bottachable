c19b853cbd0ab16490f0540876ed7bd546b8171e
Eetu Järvinen <eetu.jarvinen@cs.tamk.fi>
2017-10-09 17:08:14 +0300

    example of index page and 404 page for invalid server ids

--- discord_bottachable/views.py
+++ discord_bottachable/views.py
@@ -1,13 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from discord_bottachable.models import Link
+from discord_bottachable.models import Server, Link
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'servers': Server.objects.all()})
 
 
 def server(request, server_id):
-    links = Link.objects.filter(server_id=server_id)
-    return render(request, 'server.html', {'links': links})
+    s = get_object_or_404(Server, discord_id=server_id)
+    links = Link.objects.filter(server_id=s)
+    return render(request, 'server.html', {
+        'links': links,
+        'server_name': s.name
+    })
