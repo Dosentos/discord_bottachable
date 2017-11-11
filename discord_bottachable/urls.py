e45252e9b4bc660e62ca780bc68ed610dd0e02b3
toumas <tuomasukk@gmail.com>
2017-11-12 01:38:00 +0200

    Added channel filtering

--- discord_bottachable/urls.py
+++ discord_bottachable/urls.py
@@ -1,58 +1,95 @@
 """discord_bottachable URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+	https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+	1. Add an import:  from my_app import views
+	2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+	1. Add an import:  from other_app.views import Home
+	2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+	1. Import the include() function: from django.conf.urls import url, include
+	2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import discord_bottachable.views
 
 urlpatterns = [
-    url(r'^$', discord_bottachable.views.index, name='index'),
-    url(
-        r'^(?P<server_id>[0-9]+)/$', 
-        discord_bottachable.views.server,
-        name='server'
-    ),
-    url(
-        r'^(?P<server_id>[0-9]+)/tags/$',
-        discord_bottachable.views.server,
-        name='server'
-    ),
-    url(
-        r'^(?P<server_id>[0-9]+)/tags/(?P<tags>(\w(-)*)+(,(\w(-)*)+)*)$',
-        discord_bottachable.views.server,
-        name='server'
-    ),
-    url(
-        r'^(?P<server_id>[0-9]+)/search/$',
-        discord_bottachable.views.server,
-        name='server'
-    ),
-    url(
-        r'^(?P<server_id>[0-9]+)/search/(?P<keywords>(\w(-)*( )*)+(,(\w(-)*( )*)+)*)/$',
-        discord_bottachable.views.server,
-        name='server'
-    ),
-    url(
-        r'^(?P<server_id>[0-9]+)/search/(?P<keywords>(\w(-)*( )*)+(,(\w(-)*( )*)+)*)/tags/$',
-        discord_bottachable.views.server,
-        name='server'
-    ),
-    url(
-        r'^(?P<server_id>[0-9]+)/search/(?P<keywords>(\w(-)*( )*)+(,(\w(-)*( )*)+)*)/tags/(?P<tags>(\w(-)*)+(,(\w(-)*)+)*)$',
-        discord_bottachable.views.server,
-        name='server'
-    ),
+	url(r'^$', discord_bottachable.views.index, name='index'),
+	url(
+		r'^(?P<server_id>[0-9]+)/$', 
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/tags/$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/tags/(?P<tags>(\w(-)*)+(,(\w(-)*)+)*)$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/search/$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/search/(?P<keywords>(\w(-)*( )*)+(,(\w(-)*( )*)+)*)/$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/search/(?P<keywords>(\w(-)*( )*)+(,(\w(-)*( )*)+)*)/tags/$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/search/(?P<keywords>(\w(-)*( )*)+(,(\w(-)*( )*)+)*)/tags/(?P<tags>(\w(-)*)+(,(\w(-)*)+)*)$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/(?P<channel_name>(\w(-)*)+)/$', 
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/(?P<channel_name>(\w(-)*)+)/tags/$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/(?P<channel_name>(\w(-)*)+)/tags/(?P<tags>(\w(-)*)+(,(\w(-)*)+)*)$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/(?P<channel_name>(\w(-)*)+)/search/$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/(?P<channel_name>(\w(-)*)+)/search/(?P<keywords>(\w(-)*( )*)+(,(\w(-)*( )*)+)*)/$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/(?P<channel_name>(\w(-)*)+)/search/(?P<keywords>(\w(-)*( )*)+(,(\w(-)*( )*)+)*)/tags/$',
+		discord_bottachable.views.server,
+		name='server'
+	),
+	url(
+		r'^(?P<server_id>[0-9]+)/(?P<channel_name>(\w(-)*)+)/search/(?P<keywords>(\w(-)*( )*)+(,(\w(-)*( )*)+)*)/tags/(?P<tags>(\w(-)*)+(,(\w(-)*)+)*)$',
+		discord_bottachable.views.server,
+		name='server'
+	),
 ]
+urlpatterns += staticfiles_urlpatterns()
--- discord_bottachable/views.py
+++ discord_bottachable/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 from django.template.defaulttags import register
 from django.db.models import Q
-from discord_bottachable.models import Server, Link
+from discord_bottachable.models import Server, Link, Channel
 
 import logging
 from collections import OrderedDict
@@ -13,113 +13,130 @@ logger = logging.getLogger(__name__)
 
 @register.filter
 def get_item(dictionary, key):
-    return dictionary.get(key)
+	return dictionary.get(key)
 
 @register.filter
 def addSpaces(str):
-    return str.replace(',', ', ')
+	return str.replace(',', ', ')
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'servers': Server.objects.all()})
+	return render(request, 'index.html', {'servers': Server.objects.all()})
 
-def server(request, server_id, tags='', keywords=''):
-    if request.method == 'POST':
-        return redirect(getRedirectUrl(request, server_id, tags, keywords))
-    else:
-        keywords = str_to_array(keywords, ',')
-        s = get_object_or_404(Server, discord_id=server_id)
-        all_links = Link.objects.filter(server_id=s)
+def server(request, server_id, channel_name='', tags='', keywords=''):
+	if request.method == 'POST':
+		return redirect(getRedirectUrl(request, server_id, tags, keywords, channel_name))
+	else:
+		s = get_object_or_404(Server, discord_id=server_id)
+		all_links = Link.objects.filter(server_id=s)
+		
+		channels = Channel.objects.filter(server_id=s.id)
 
-        tags = str_to_array(tags, ',')
+		if len(channel_name) > 0:
+			all_links = filterByChannel(all_links, channels, channel_name)
 
-        if len(keywords) > 0:
-            all_links = filterByKeywords(all_links, keywords)
-        
-        if len(tags) > 0:
-            all_links = filterByTags(all_links, tags)
+		keywords = str_to_array(keywords, ',')
 
-        paginator = Paginator(all_links, 10)
+		if len(keywords) > 0:
+			all_links = filterByKeywords(all_links, keywords)
+		
+		tags = str_to_array(tags, ',')
 
-        page = request.GET.get('page')
-        try:
-            links = paginator.page(page)
-        except PageNotAnInteger:
-            links = paginator.page(1)
-        except EmptyPage:
-            links = paginator.page(paginator.num_pages)
+		if len(tags) > 0:
+			all_links = filterByTags(all_links, tags)
 
-        return render(request, 'server.html', {
-            'links': links,
-            'server_id': server_id,
-            'server_name': s.name,
-            'tags': tags,
-            'current_tags': ','.join(tags),
-            'current_keywords': ','.join(keywords),
-            'tag_links': getTagLinks(all_links, server_id, tags, keywords)
-        })
+		paginator = Paginator(all_links, 10)
 
-def getRedirectUrl(request, server_id, tags, keywords):
-    # convert to array
-    keywords = str_to_array(request.POST.get('keywords', None), ',')
-    # trim every keyword
-    for i, keyword in enumerate(keywords):
-        keywords[i] = keyword.strip()
-    # convert back to string
-    keywords = ','.join(keywords)
+		page = request.GET.get('page')
+		try:
+			links = paginator.page(page)
+		except PageNotAnInteger:
+			links = paginator.page(1)
+		except EmptyPage:
+			links = paginator.page(paginator.num_pages)
 
-    url = '/' + server_id + '/'
+		return render(request, 'server.html', {
+			'links': links,
+			'server_id': server_id,
+			'server_name': s.name,
+			'channels': channels,
+			'tags': tags,
+			'current_tags': ','.join(tags),
+			'current_keywords': ','.join(keywords),
+			'tag_links': getTagLinks(all_links, server_id, tags, keywords, channel_name)
+		})
 
-    if len(keywords) > 0:
-        url += 'search/' + keywords + '/'
-    if len(tags) > 0:
-        url += 'tags/' + ','.join(str_to_array(tags, ','))
+def getRedirectUrl(request, server_id, tags, keywords, channel_name):
+	# convert to array
+	keywords = str_to_array(request.POST.get('keywords', None), ',')
+	# trim every keyword
+	for i, keyword in enumerate(keywords):
+		keywords[i] = keyword.strip()
+	# convert back to string
+	keywords = ','.join(keywords)
 
-    return url
+	url = '/' + server_id + '/'
 
-def getTagLinks(all_links, server_id, tags, keywords):
-    urls = {}
-    for l in all_links:
-        url = ''
-        urls[l.id] = {}
-        for t in l.tags.all():
-            url = ''
-            if len(keywords) > 0:
-                url = 'search/' + ','.join(keywords) + '/'
-            if len(tags) > 0:
-                if t.name not in tags:
-                    url += 'tags/' + ','.join(tags) + ',' + t.name
-                else:
-                    url += 'tags/' + ','.join(tags)
-            elif len(tags) == 0:
-                url += 'tags/' + t.name
-            urls[l.id][t.id] = url
-    return urls
+	if len(channel_name) > 0:
+		url += channel_name + '/'
+	if len(keywords) > 0:
+		url += 'search/' + keywords + '/'
+	if len(tags) > 0:
+		url += 'tags/' + ','.join(str_to_array(tags, ','))
+
+	return url
+
+def getTagLinks(all_links, server_id, tags, keywords, channel_name):
+	urls = {}
+	for l in all_links:
+		url = ''
+		urls[l.id] = {}
+		for t in l.tags.all():
+			url = ''
+			if len(channel_name) > 0:
+				url += channel_name + '/'
+			if len(keywords) > 0:
+				url = 'search/' + ','.join(keywords) + '/'
+			if len(tags) > 0:
+				if t.name not in tags:
+					url += 'tags/' + ','.join(tags) + ',' + t.name
+				else:
+					url += 'tags/' + ','.join(tags)
+			elif len(tags) == 0:
+				url += 'tags/' + t.name
+			urls[l.id][t.id] = url
+	return urls
+
+def filterByChannel(all_links, channels, channel_name):
+	channel = channels.filter(name=channel_name)
+	if len(channel) > 0:
+		all_links = all_links.filter(channel_id=channel[0].id)
+	return all_links
 
 def filterByKeywords(all_links, keywords):
-    for keyword in keywords: 
-        all_links = all_links.filter(
-            Q(title__icontains=keyword) | Q(description__icontains=keyword)
-        )
+	for keyword in keywords: 
+		all_links = all_links.filter(
+			Q(title__icontains=keyword) | Q(description__icontains=keyword)
+		)
 
-    return all_links
+	return all_links
 
 def filterByTags(all_links, tags):
-    for tag in tags: 
-        all_links = all_links.filter(tags__name=tag)
+	for tag in tags: 
+		all_links = all_links.filter(tags__name=tag)
 
-    return all_links
+	return all_links
 
 def str_to_array(str, delimeter):
-    try:
-        # Because splitting an empty string still returns an array containing
-        # an empty string
-        if len(str) > 0:
-            array = str.split(delimeter)
-            # Remove duplicates
-            array = list(OrderedDict((item, True) for item in array).keys())
-        else:
-            array = []
-    except Exception as e:
-        array = []
-    return array
+	try:
+		# Because splitting an empty string still returns an array containing
+		# an empty string
+		if len(str) > 0:
+			array = str.split(delimeter)
+			# Remove duplicates
+			array = list(OrderedDict((item, True) for item in array).keys())
+		else:
+			array = []
+	except Exception as e:
+		array = []
+	return array
