fea5a179878a64cf8382822bd80671fe266c9092
Viljami <ketola.viljami@gmail.com>
2017-11-15 16:34:52 +0200

    Removed test functions/commands

--- discord_bottachable/management/commands/bot.py
+++ discord_bottachable/management/commands/bot.py
@@ -228,18 +228,6 @@ async def handle_messages(message):
         return
 
     # Other normie commands:
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = await client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-
-    elif message.content.startswith('!sleep'):
-        await asyncio.sleep(5)
-        await client.send_message(message.channel, 'Done sleeping')
-
     elif message.content.startswith('!link'):
         message_saved = False
         error_message = ''
